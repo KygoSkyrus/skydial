e85da99aef44ed0f6a7c01b831076243844eda1b
Dheeraj Gupta <78816789+KygoSkyrus@users.noreply.github.com>
2024-03-05 16:20:07 +0530

    css changes

--- client/src/DialPage.js
+++ client/src/DialPage.js
@@ -46,7 +46,7 @@ const DialPage = () => {
             setLocalStream(stream);
             myVideoRef.current.srcObject = stream;
         }
-        startCall()
+        // startCall()
         // const uId = uuidv4();
 
 
@@ -153,7 +153,36 @@ const DialPage = () => {
                         <video ref={remoteVideoRef} playsInline autoPlay className='border border-gray-400 rounded-lg shadow-sm'></video>
                         :
                         null}
-                    <video ref={myVideoRef} playsInline autoPlay muted className='border border-gray-400 rounded-lg shadow-sm'></video>
+                    <div className='w-full h-full relative'>
+                        <video ref={myVideoRef} playsInline autoPlay muted className='w-full h-full border border-gray-400 rounded-lg shadow-sm'></video>
+
+                        <div className='w-full absolute bottom-4 flex justify-center items-center gap-3'>
+                            <section className='border hover:border-gray-900 border-gray-500 text-white p-2 cursor-pointer rounded-2xl px-3'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+                                </svg>
+
+                            </section>
+
+                            <section className='border hover:border-gray-900 border-gray-500 text-white p-2 cursor-pointer rounded-2xl px-3'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                </svg>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hidden">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M12 18.75H4.5a2.25 2.25 0 0 1-2.25-2.25V9m12.841 9.091L16.5 19.5m-1.409-1.409c.407-.407.659-.97.659-1.591v-9a2.25 2.25 0 0 0-2.25-2.25h-9c-.621 0-1.184.252-1.591.659m12.182 12.182L2.909 5.909M1.5 4.5l1.409 1.409" />
+                                </svg>
+                            </section>
+
+                            <section className='bg-red-600 text-white p-2 cursor-pointer rounded-2xl px-3'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 3.75 18 6m0 0 2.25 2.25M18 6l2.25-2.25M18 6l-2.25 2.25m1.5 13.5c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                                </svg>
+                            </section>
+                        </div>
+                    </div>
+
+
                 </div>
 
 
--- notes.md
+++ notes.md
@@ -23,3 +23,14 @@
   - when clicked on 'join' button, user should be redirected to Join page
   - on the page initially they'll get a modal asking  for their name and callersId 
     - if both fields are filled then 
+
+# without login
+- **create a call**: 
+  - when clicked on start a call user will get a modal dialog to enter his name and then he will be redirected to call page
+  - on call page he will have a invite button to this video call. this button should have  a link that will bring other user to this call, {need the rest of logic in join call}
+  - on call page he will be shown his video and there will be a chat window of width 25% side to the video(75%),
+  - this chat window will show the user only as he is the only one
+
+- **join a call**: 
+ - same as above when clicked on join the user will get the same modal for name but along with the he would need to enter the room id,
+ - after he enters these things than he will be redirected to the room, and in the room there should be a useefect which will run with the 'answercall' and  it will have the user id, maybe instead of uuid roomid in the share url, it should be user's socket id(which is the main requirement for a connection)
